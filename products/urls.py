9b8aa123f07ac7bf10d3ec8f850c70a39918b2b7
Alan <115167745+alaingb123@users.noreply.github.com>
2024-08-16 12:22:52 -0400

    algunos cambios minisculos por aqui por alla

--- products/urls.py
+++ products/urls.py
@@ -7,9 +7,12 @@ urlpatterns = [
     path('create/', views.product_create_view, name='create'),
     path('mis_productos_table/', views.mis_productos_table, name='mis_productos'),
     path('', views.product_list_view, name='list'),
+
     path('<slug:handle>/', views.product_detail_view, name='detail'),
     path('<slug:handle>/manage/', views.product_manage_detail_view, name='manage'),
     path('<slug:handle>/download/<int:pk>', views.product_attachment_download_view, name='download'),
+
+    path('search/', views.search, name='search'),
 
     # path('eliminar/<slug:handle>/', views.delete_product, name='eliminar'),
 
