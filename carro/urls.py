b1cddd8bdd1d208c6b2030e1f7d7dc04ce06ce81
Alan <115167745+alaingb123@users.noreply.github.com>
2024-08-18 15:18:10 -0400

    actualizacion del arrito en vez de una ventana lo pusimos en una pagina aparte

--- carro/urls.py
+++ carro/urls.py
@@ -5,8 +5,12 @@ from . import views
 app_name = 'carro'
 urlpatterns = [
     path('agregar/<int:product_id>/', views.agregar_producto, name='agregar'),
+    path('agregar_carro/<int:product_id>/', views.agregar_producto_desde_carro, name='agregar_carro'),
     path('eliminar/<int:product_id>/', views.eliminar_producto, name='eliminar'),
     path('restar/<int:product_id>/', views.restar_producto, name='restar'),
     path('limpiar/', views.limpiar_carro, name='limpiar'),
+    path('carro/', views.ver_carro, name='ver_carro'),
+
+
 
 ]
--- carro/views.py
+++ carro/views.py
@@ -14,15 +14,17 @@ def agregar_producto(request,product_id):
     return redirect("products:list")
 
 
-# def agregar_producto(request,product_id):
-#     carro = Carro(request)
-#     product = Product.objects.get(pk=product_id)
-#
-#     quantity = int(request.POST.get('quantity', 1))
-#     print(" la cantidad es : ", quantity)
-#     carro.agregar(product=product,quantity=quantity)
-#
-#     return JsonResponse(carro.carro)
+def agregar_producto_desde_carro(request,product_id):
+    carro = Carro(request)
+    product = Product.objects.get(pk=product_id)
+
+    quantity = int(request.POST.get('quantity', 1))
+    print(" la cantidad es : ", quantity)
+    carro.agregar(product=product, quantity=quantity)
+
+    return redirect("carro:ver_carro")
+
+
 
 
 
@@ -32,7 +34,7 @@ def restar_producto(request, product_id):
 
     carro.restar_product(product=product)
 
-    return redirect("products:list")
+    return redirect("carro:ver_carro")
 
 
 def limpiar_carro(request):
@@ -40,16 +42,18 @@ def limpiar_carro(request):
 
     carro.limpiar_carro()
 
-    return redirect("products:list")
+    return redirect("carro:ver_carro")
 
 def eliminar_producto(request,product_id):
     carro = Carro(request)
     product = Product.objects.get(pk=product_id)
     carro.eliminar(product)
 
-    return redirect("products:list")
+    return redirect("carro:ver_carro")
 
 
+def ver_carro(request):
+    return render(request,"purchases/cart.html")
 
 
 
