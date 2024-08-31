bbcd9b44dcffa23401e654ff7e01214829cc7419
naomiapc <n.alison.pedroso@gmail.com>
2024-08-31 19:55:27 +0200

    So so el editar usuario

--- usuario/forms.py
+++ usuario/forms.py
@@ -28,7 +28,12 @@ class FormularioActualizarUsuario(UserChangeForm):
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
 
-    # def __init__(self, *args, **kwargs):
-    #     super(FormularioActualizarUsuario, self).__init__(*args, **kwargs)
-    #     for field in self.fields:
-    #         self.fields[field].widget.attrs['class'] = input_css_class
+    def __init__(self, *args, **kwargs):
+        super(FormularioActualizarUsuario, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            # Añadir clases CSS a cada campo
+            self.fields[field].widget.attrs.update({
+                'class': 'input',  # Clase para aplicar estilos
+                'placeholder': field.label,  # Placeholder con el nombre del campo
+                'style': 'border: 1px solid var(--davys-gray); border-radius: 5px; padding: 10px; width: 100%; height: 3rem; font-size: 1em; transition: border-color 0.3s;'
+            })
