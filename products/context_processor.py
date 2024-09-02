6c1e2a2192aafb38bf0c24080109cb830e49cb64
Alan <115167745+alaingb123@users.noreply.github.com>
2024-09-02 13:22:59 -0400

    arreglado la malformacion del template y el error de anonimo user comprobando si el usuario esta autenticado en el context porcesor de cantidad de like

--- products/context_processor.py
+++ products/context_processor.py
@@ -4,8 +4,11 @@ from products.models import Product
 
 
 def cantidad_like(request):
-    liked_products = Product.objects.filter(like__user=request.user)
-    cantidad_like = liked_products.count()
+    if request.user.is_authenticated:
+        liked_products = Product.objects.filter(like__user=request.user)
+        cantidad_like = liked_products.count()
+    else:
+        cantidad_like=0
     return {'cantidad_like': cantidad_like}
 
 
